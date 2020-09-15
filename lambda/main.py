ad161b514b894189906f27271146a963f3c420f7
Jordan Matelsky <j6k4m8@gmail.com>
2020-09-15 11:40:47 -0400

    Add find-motifs as single-step fn

--- lambda/main.py
+++ lambda/main.py
@@ -2,20 +2,64 @@ import json
 import os
 
 import boto3
+import networkx as nx
 
 from grand.backends import DynamoDBBackend
+import grand
+
+from grandiso import get_next_backbone_candidates, uniform_node_interestingness
+
+"""
+def get_next_backbone_candidates(
+    backbone: dict,
+    motif: nx.Graph,
+    host: nx.Graph,
+    interestingness: dict,
+    next_node: str = None,
+    directed: bool = True,
+    enforce_inequality: bool = True,
+) -> List[dict]:
+"""
 
 # LOCALSTACK_HOSTNAME = os.getenv("LOCALSTACK_HOSTNAME")
 LOCALSTACK_HOSTNAME = "172.17.0.1"
+LOCALSTACK_HOSTNAME = "localhost"
 
 ENDPOINT_URL = "http://" + LOCALSTACK_HOSTNAME + ":4566"
 
 
+def find_motifs_step(
+    motif: nx.Graph, backbone: dict, host: grand.Graph, interestingness=None
+):
+    interestingness = interestingness or uniform_node_interestingness(motif)
+
+    if isinstance(motif, nx.DiGraph):
+        # This will be a directed query.
+        directed = True
+    else:
+        directed = False
+
+    next_candidate_backbones = get_next_backbone_candidates(
+        backbone, motif, host, interestingness, directed=directed
+    )
+
+    results = []
+    for candidate in next_candidate_backbones:
+        if len(candidate) == len(motif):
+            results.append(candidate)
+        else:
+            # q.put(candidate)
+            print(candidate)
+
+    print(results)
+    return results
+
+
 def main(event, lambda_context):
 
-    DG = DynamoDBBackend(dynamodb_url=ENDPOINT_URL)
+    DG = grand.Graph(backend=DynamoDBBackend(dynamodb_url=ENDPOINT_URL))
 
-    print(DG.get_node_count())
+    print(len(DG.nx))
 
     # sqs_client = boto3.client(
     #     "sqs",
@@ -42,3 +86,7 @@ def main(event, lambda_context):
     result = json.dumps({"candidate": candidate})
     print(result)
     return result
+
+
+if __name__ == "__main__":
+    main(0, 0)
